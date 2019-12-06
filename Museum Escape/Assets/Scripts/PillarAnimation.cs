83245b51ce660c5f683768c3148c67de471127c2
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2019-12-06 12:56:05 +0000

    Getting closer
    Made models for:
    key
    Clock
    working on globe
    Lock disappears

---
+++ Museum Escape/Assets/Scripts/PillarAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarAnimation : MonoBehaviour
+{
+
+    Animator animator;
+    public float InputX;
+    public float InputY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the animator
+        animator = this.gameObject.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
