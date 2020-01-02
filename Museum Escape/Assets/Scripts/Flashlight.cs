6a96ac662f33d2ba443e85b9471587b6146c5ab8
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2020-01-02 17:15:05 +0000

    First day back
    Made a wood texture will mess with channels later
    Made flashlight turn off and on
    felt like a failure = constant

---
+++ Museum Escape/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject Torch;
+    void Start()
+    {
+        Torch = GameObject.Find("Torch");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && Torch.active == true)
+        {
+            Torch.SetActive(false);
+        }
+        else if(Input.GetKeyDown(KeyCode.F) && Torch.active == false)
+        {
+            Torch.SetActive(true);
+        }
+    }
+}
