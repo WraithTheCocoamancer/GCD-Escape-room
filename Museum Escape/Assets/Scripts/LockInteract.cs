1049636fc33ada07ef0f06ddfb7b3e576c22ccd1
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2020-01-04 18:25:42 +0000

    Another good day
    More textures made
    more models made
    Pillar turns over

--- Museum Escape/Assets/Scripts/LockInteract.cs
+++ Museum Escape/Assets/Scripts/LockInteract.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class LockInteract : MonoBehaviour
 {
     // Start is called before the first frame update
+    private GameObject Inv;
+    private bool HaveKey = false;
+    private GameObject Key;
+    private GameObject Lock;
     void Start()
     {
-        
+        Key = GameObject.Find("Key");
+        Inv = GameObject.Find("Inventory");
+        Lock = GameObject.Find("lock");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void ReactToRaycast()
+    {
+        HaveKey = Inv.GetComponent<Inventory>().KeyGot;
+        if (HaveKey == true)
+        {
+            Lock.SetActive(false);
+            Inv.GetComponent<Inventory>().LockGone = true;
+        }
+
+        else
+        {
+            Debug.Log("Every lock needs a key...unless it's combination but this one definately needs a key..orrrr lockpicks..nah key.");
+        }
     }
 }
--- Museum Escape/Assets/Scripts/PillarInteract.cs
+++ Museum Escape/Assets/Scripts/PillarInteract.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PillarInteract : MonoBehaviour
 {
+
     // Start is called before the first frame update
+    public GameObject GemEye;
+    private Animator anim;
+    private GameObject Hinge;
+    private GameObject Inv;
+    private bool LockGone = false;
+    public GameObject Key;
     void Start()
     {
-        
+        Hinge = GameObject.Find("Hinge");
+        Inv = GameObject.Find("Inventory");
+        anim = Hinge.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void ReactToRaycast()
+    {
+        LockGone = Inv.GetComponent<Inventory>().LockGone;
+        if (LockGone == true)
+        {
+            
+            anim.Play("PillarTurn");
+            
+        }
+
+        else
+        {
+            Debug.Log("Hmnn something could go in here");
+        }
     }
 }
