e966210fe0944e60b765bdaf5272c18faf64f176
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2020-01-06 16:25:02 +0000

    Buttons. am going to lunch

--- Museum Escape/Assets/Scripts/Button1Interact.cs
+++ Museum Escape/Assets/Scripts/Button1Interact.cs
@@ -26,11 +26,24 @@ public class Button1Interact : MonoBehaviour
     }
     void ReactToRaycast()
     {
-    Button1 = ButtTrack.GetComponent<ButtonTracking>().
-    Button2 =
-    Button3 = 
-    Button4 = 
-    button5 = 
-    Button6 = 
-}
+        Button1 = ButtTrack.GetComponent<ButtonTracking>().Button1;
+        Button2 = ButtTrack.GetComponent<ButtonTracking>().Button2;
+        Button3 = ButtTrack.GetComponent<ButtonTracking>().Button3;
+        Button4 = ButtTrack.GetComponent<ButtonTracking>().Button4;
+        Button5 = ButtTrack.GetComponent<ButtonTracking>().Button5;
+        Button6 = ButtTrack.GetComponent<ButtonTracking>().Button6;
+        if (Button1 == false && Button2 == false && Button3 == false && Button4 == false && Button5 == false && Button6 == false)
+        {
+            ButtTrack.GetComponent<ButtonTracking>().Button1 = true;
+        }
+        else
+        {
+            ButtTrack.GetComponent<ButtonTracking>().Button1 = false;
+            ButtTrack.GetComponent<ButtonTracking>().Button2 = false;
+            ButtTrack.GetComponent<ButtonTracking>().Button3 = false;
+            ButtTrack.GetComponent<ButtonTracking>().Button4 = false;
+            ButtTrack.GetComponent<ButtonTracking>().Button5 = false;
+            ButtTrack.GetComponent<ButtonTracking>().Button6 = false;
+        }
+    }
 }
--- Museum Escape/Assets/Scripts/ButtonTracking.cs
+++ Museum Escape/Assets/Scripts/ButtonTracking.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class ButtonTracking : MonoBehaviour
 {
     // Start is called before the first frame update
-    private bool Button1 = false;
-    private bool Button2 = false;
-    private bool Button3 = false;
-    private bool Button4 = false;
-    private bool Button5 = false;
-    private bool Button6 = false;
+    public bool Button1 = false;
+    public bool Button2 = false;
+    public bool Button3 = false;
+    public bool Button4 = false;
+    public bool Button5 = false;
+    public bool Button6 = false;
     void Start()
     {
         
